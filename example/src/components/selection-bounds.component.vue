0bcb29d9df1e81f8c148c018231198966ee2736d
benoitlahoz <lahoz.ben@gmail.com>
2024-05-03 10:12:39 +0200

    Create selection-bounds.component.vue

---
+++ example/src/components/selection-bounds.component.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export default {
+  name: 'SelectionBounds',
+};
+</script>
+<script setup lang="ts">
+const props = defineProps({
+  row: {
+    type: Number,
+    default: -1,
+  },
+  col: {
+    type: Number,
+    default: -1,
+  },
+  height: {
+    type: Number,
+    default: 0,
+  },
+  width: {
+    type: Number,
+    default: 0,
+  },
+});
+</script>
+<template lang="pug">
+div SelectionBounds component
+</template>
+<style lang="sass" scoped></style>
